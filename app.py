9cb77b231f81a77acacdfa5361c10734909a00a1
Akashvarma26 <akashvdatla@gmail.com>
2025-07-01 22:36:59 +0530

    Deployment v3

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ origins = [
     # frontends urls here
     "http://localhost:5500",
     "http://127.0.0.1:5500",
+    "https://akashvarma26.github.io/PitLaneInsiders"
     "https://www.pitlaneinsiders.site"
 ]
 
@@ -59,6 +60,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+def root():
+    print("Pinged!")
+    return {"message": "Service alive"}
+
 
 # To display real time news data in the frontend
 @app.get("/news")
